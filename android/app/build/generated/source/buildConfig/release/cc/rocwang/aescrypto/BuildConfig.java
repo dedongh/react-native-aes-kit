a4e61d3d91b4cdc3a036de690c2e54c679741f7d
Bra Emma <slimshades68@gmail.com>
2025-04-09 15:37:11 +0000

    added pobspec file

---
+++ android/app/build/generated/source/buildConfig/release/cc/rocwang/aescrypto/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package cc.rocwang.aescrypto;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "cc.rocwang.aescrypto";
+  public static final String BUILD_TYPE = "release";
+}
